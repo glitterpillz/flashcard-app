af54dbef211c4086aefae94f8e293ce245e90e9c
glitterpillz <karenhickey1010@gmail.com>
2025-02-19 19:09:17 -0500

    redeploy

--- src/app/edit-flashcard-set/page.js
+++ src/app/edit-flashcard-set/page.js
@@ -1,8 +1,3 @@
-// import CreateFlashcardSet from "../create-flashcard-set/page";
-
-// export default function EditFlashcardSetPage() {
-//     return <CreateFlashcardSet />;
-// }
 import CreateFlashcardSet from "../create/page";
 
 export default function EditFlashcardSetPage() {
